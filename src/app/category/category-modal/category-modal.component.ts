0c01a0ef723bb5298cb7d38326da432cc7aa44cf
dloc16 <dennis.loconte@lernen.siw.swiss>
2026-04-21 22:16:13 +0200

    feat: category-modal statisch mit cancel/save/delete aufgebaut

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -1,5 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { ModalController } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonIcon,
+  IonInput,
+  IonFab,
+  IonFabButton,
+  ModalController
+} from '@ionic/angular/standalone';
 import { ReactiveFormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { close, save, text, trash } from 'ionicons/icons';
@@ -7,21 +20,32 @@ import { close, save, text, trash } from 'ionicons/icons';
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
-  imports: [ReactiveFormsModule]
+  imports: [
+    ReactiveFormsModule,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonIcon,
+    IonInput,
+    IonFab,
+    IonFabButton
+  ]
 })
 export default class CategoryModalComponent {
   // DI
   private readonly modalCtrl = inject(ModalController);
 
   // Lifecycle
-
   constructor() {
     // Add all used Ionic icons
     addIcons({ close, save, text, trash });
   }
 
   // Actions
-
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
