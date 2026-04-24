6b43ba0ff456991942d9fd8a3d494d5153c5a335
dloc16 <dennis.loconte@lernen.siw.swiss>
2026-04-24 19:29:08 +0200

    feat: kategorien-liste mit skeleton, progress bar und auto-load

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,29 +1,35 @@
 import { Component, inject } from '@angular/core';
 import {
-  IonHeader,
-  IonToolbar,
   IonButtons,
-  IonMenuButton,
-  IonTitle,
-  IonContent,
-  IonGrid,
-  IonRow,
   IonCol,
-  IonItem,
-  IonIcon,
-  IonSelect,
-  IonSelectOption,
-  IonInput,
-  IonList,
-  IonLabel,
+  IonContent,
   IonFab,
   IonFabButton,
-  ModalController
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonMenuButton,
+  IonProgressBar,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonSkeletonText,
+  IonTitle,
+  IonToolbar,
+  ModalController,
+  ViewDidEnter
 } from '@ionic/angular/standalone';
 import { ReactiveFormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { add, alertCircleOutline, search, swapVertical } from 'ionicons/icons';
+import { finalize } from 'rxjs';
 import CategoryModalComponent from '../category-modal/category-modal.component';
+import { CategoryService } from '../category.service';
+import { ToastService } from '../../shared/service/toast.service';
+import { Category, CategoryCriteria } from '../../shared/domain';
 
 @Component({
   selector: 'app-category-list',
@@ -44,27 +50,62 @@ import CategoryModalComponent from '../category-modal/category-modal.component';
     IonSelect,
     IonSelectOption,
     IonInput,
-    IonList,
     IonLabel,
     IonFab,
-    IonFabButton
+    IonFabButton,
+    IonProgressBar,
+    IonSkeletonText
   ]
 })
-export default class CategoryListComponent {
+export default class CategoryListComponent implements ViewDidEnter {
   // DI
+  private readonly categoryService = inject(CategoryService);
   private readonly modalCtrl = inject(ModalController);
+  private readonly toastService = inject(ToastService);
+
+  // Variables
+  categories: Category[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: CategoryCriteria = { page: 0, size: 25, sort: this.initialSort };
 
   // Lifecycle
   constructor() {
-    // Add all used Ionic icons
     addIcons({ swapVertical, search, alertCircleOutline, add });
+  }
+
+  ionViewDidEnter(): void {
+    this.loadCategories();
   }
 
   // Actions
   async openModal(): Promise<void> {
     const modal = await this.modalCtrl.create({ component: CategoryModalComponent });
-    modal.present();
+    void modal.present();
     const { role } = await modal.onWillDismiss();
     console.log('role', role);
+  }
+
+  // Helpers
+  private loadCategories(next?: () => void): void {
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    this.categoryService
+      .getCategories(this.searchCriteria)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          if (next) next();
+        })
+      )
+      .subscribe({
+        next: categories => {
+          if (this.searchCriteria.page === 0 || !this.categories) this.categories = [];
+          this.categories.push(...categories.content);
+          this.lastPageReached = categories.last;
+        },
+        error: error => this.toastService.displayWarningToast('Could not load categories', error)
+      });
   }
 }
